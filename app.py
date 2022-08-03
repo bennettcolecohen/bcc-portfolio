59a8811a52b05c16785f508e26d99c33aee2c524
Bennett Cohen <bennettcohen@Bennetts-MacBook-Air.local>
2022-08-03 11:24:50 -0700

    sdf

--- app.py
+++ app.py
@@ -7,17 +7,6 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-# def connect_to_db(): 
-#     try: 
-#         engine = create_engine(
-#             "postgres://uecxoerwcedqby:10eaaf94acd25999e959949659a95e33cc3766802e01d5990e6e38f50ba740ec@ec2-34-235-31-124.compute-1.amazonaws.com:5432/d94n2pv9om0d09", 
-#             connect_args = {'connect_timeout': 10}, 
-#             echo = False, pool_size = 20, max_overflow = 0)
-#         print('Connection Initiated')
-#     except:
-#         raise ValueError("Can't connect to Heroku PostgreSQL! You must be so embarrassed")
-#     return engine
-
 @app.get("/")
 def index(request: Request): 
     return templates.TemplateResponse("index.html", {"request": request})
@@ -30,19 +19,9 @@ def projects(request: Request):
 def projects(request: Request): 
     return templates.TemplateResponse("blog.html", {"request": request})
 
-
-
-# @app.get("/projects")
-# def projects(request: Request): 
-#     return templates.TemplateResponse("projects.html", {"request": request})
-
-# @app.get('/blog')
-# def blog(request: Request): 
-#     return templates.TemplateResponse('blog.html', {"request": request})
-
-# @app.get("/projects/{project_name}")
-# def betml(project_name: str, request: Request): 
-#     return templates.TemplateResponse(f"{project_name}.html", {"request": request})
+@app.get("/projects/{project_name}")
+def betml(project_name: str, request: Request): 
+    return templates.TemplateResponse(f"projects/{project_name}.html", {"request": request})
 
 # @app.get("/header")
 # def header(request: Request): 
